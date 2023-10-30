d4cb51be8d5e9f14c55e9a7328a08a1c7e6d82dd
Mihir Khare <mihir00khare@gmail.com>
2023-10-30 13:29:23 -0400

    Added accuracy to the desired nondim temperature

--- src/headers/main.h
+++ src/headers/main.h
@@ -13,9 +13,9 @@ const int steps = 500 * 200;
 const double sidelen = 6.8L;
 
 /* Scaling factor for random velocity generation */
-const double velstd = 0.1;
+const double velstd = 0.70710678118L;
 
-#define CUTOFF 1L
+#define CUTOFF 2.5L
 #define CUTOFF6 (CUTOFF * CUTOFF * CUTOFF * CUTOFF * CUTOFF * CUTOFF)
 /* Cutoff distance for force/potential */
 const double cutoff = CUTOFF;
@@ -32,6 +32,6 @@ const double pcut = 4.0 / (CUTOFF6 * CUTOFF6) - 4.0 / CUTOFF6;
 const double tdamp = 0.05;
 
 /* Desired system temperature */
-const double tempdes = 0.8L;
+const double tempdes = 1.38L / 1.66L;  // 100 K for argon
 
 #endif  // MAIN_H
